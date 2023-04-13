88674bd363a41387f5d23ffb83007dea2d886119
MrCarbene <Muhammad_Fouda@proton.me>
2023-04-13 11:05:28 +0200

    add login service to auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
 import { RegisterDto } from './dtos/register.dto';
 
 @Controller('auth')
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   register(@Body() registerData: RegisterDto) {
     return this.authService.register(registerData);
+  }
+
+  @Post('login')
+  login(@Body() loginData: LoginDto) {
+    return this.authService.login(loginData);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterDto } from './dtos/register.dto';
 import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -34,6 +35,33 @@ export class AuthService {
       // return user without password
       delete newUser.password;
       return newUser;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async login({ email, password }: LoginDto) {
+    try {
+      // check if user exists
+      const user = await this.userRepo.findOne({ where: { email } });
+
+      if (!user)
+        throw new HttpException(
+          `You're not registered, please register first!`,
+          HttpStatus.FORBIDDEN,
+        );
+      // check if password is correct
+      const passwordMatches = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatches)
+        throw new HttpException('incorrect password', HttpStatus.FORBIDDEN);
+
+      // TODO: GENERATE TOKEN
+
+      // Return User data with token
+      delete user.password;
+
+      return { user, token: 'ToBeGenerated' };
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
